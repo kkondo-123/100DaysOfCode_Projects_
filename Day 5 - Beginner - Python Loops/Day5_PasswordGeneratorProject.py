68802bdd1038ac4b4f38287550c36c00f14fbe94
kkondo-123 <unicorndjtokyo@gmail.com>
2025-06-13 17:45:46 -0400

    Update and rename Day5_PasswordGeneratorProject.py to Day 5 - Beginner - Python Loops/Day5_PasswordGeneratorProject.py

--- Day 5 - Beginner - Python Loops/Day5_PasswordGeneratorProject.py
+++ Day 5 - Beginner - Python Loops/Day5_PasswordGeneratorProject.py
@@ -1,3 +1,10 @@
+"""
+Name: 
+Date: 2025/05/
+Course: 100 Days of Code Day 5
+Description: Random Password Generator
+"""
+
 import random
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
