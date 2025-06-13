bebf535e0a1d4089ea65218fd5d62217feda70a9
kkondo-123 <unicorndjtokyo@gmail.com>
2025-06-13 17:53:51 -0400

    Update Day6_EscapeMaze-Reeborg.py

--- Day6_EscapeMaze-Reeborg.py
+++ Day6_EscapeMaze-Reeborg.py
@@ -1,3 +1,10 @@
+"""
+Name:
+Date: 2025/06/04
+Course: 100 Days of Code Day 6
+Description: How to escape maze with additional maze challenges from Angela
+"""
+
 # Link to Reeborg's world: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
 
 # My solution
