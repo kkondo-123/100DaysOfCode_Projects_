652ee6d86b9c08b231b167bdd774623caec9d27c
kkondo-123 <unicorndjtokyo@gmail.com>
2025-06-13 17:46:58 -0400

    Create day-5-start.py

---
+++ Day 5 - Beginner - Python Loops/day-5-start.py
@@ -0,0 +1,11 @@
+"""
+Name:
+Date: 2022/08/24
+Course: 100 Days of Code Day 5
+Description: 
+"""
+
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits: 
+  print(fruit)
+  print(fruit + "pie")
