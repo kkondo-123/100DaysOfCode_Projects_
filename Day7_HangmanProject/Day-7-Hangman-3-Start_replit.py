d9b29dfdb23d8f2bc6d651ae11eec0a4dcb138c9
kkondo-123 <unicorndjtokyo@gmail.com>
2025-06-13 20:50:57 -0400

    Create Day-7-Hangman-3-Start_replit.py

---
+++ Day7_HangmanProject/Day-7-Hangman-3-Start_replit.py
@@ -0,0 +1,40 @@
+"""
+Name: 
+Date: 2020/05/22 - 2022/08/26
+Course: 100 Days of Code Day 7
+Description: Hangman Project
+"""
+
+#Step 3
+
+import random
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+#TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
+
+end_of_game = False
+
+while end_of_game == False:
+    guess = input("Guess a letter: ").lower()
+  #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+    print(display)
+    if "_" not in display:
+      end_of_game = True
+      print("You Won!!")
+
+  
