0c07b79e9f9bb49ad47aba5c8b97b728ac987f97
kkondo-123 <unicorndjtokyo@gmail.com>
2025-07-26 17:26:40 -0400

    Edit and rename files

---
+++ Day 1 - Beginner - Working with Variables in Python to Manage Data/Day1_BandNameGeneratorProject.py
@@ -0,0 +1,18 @@
+"""
+Name: Kana Kondo
+Date: 2025-07-26-Sat
+Course: 100 Days of Code Day 1
+Description: Band Name Generator
+"""
+
+#1. Create a greeting for your program.
+print ("Welcome to the Band Name Generator \nThis generates a band name based on the city that you grew up in and the name of a pet.")
+
+#2. Ask the user for the city that they grew up in.
+city_name = input("What's the name of the city you grew up in?\n")
+
+#3. Ask the user for the name of a pet.
+pet_name = input("What's your pet's name?\n")
+
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band name could be: " + city_name + " " + pet_name)
--- Day 2 - Beginner - Understanding Data Types and How to Manipulate Strings/Day2_TipCalculator.py
+++ Day 2 - Beginner - Understanding Data Types and How to Manipulate Strings/Day2_TipCalculator.py
@@ -1,20 +1,10 @@
 """
 Name: Kana Kondo
-Date: 2025/05/23
+Date: 2025-05-23
 Course: 100 Days of Code Day 2
 Description: Tip Calculator Proejct
 """
 
-# Solution 1
-print("Welcome to the tip calculator!")
-bill = float(input("What was the total bill? $"))
-tip = int(input("What percentage tip would you like to give? 10 12 15 "))
-people = int(input("How many people to split the bill? "))
-each_person = bill * (1 + tip * 0.01) / people
-rounded_num = round(each_person, ndigits=2)
-print(f"Each person should pay: $ {rounded_num}")
-
-# Solution 2
 print("Welcome to the tip calculator!")
 total_bill = input("What was the total bill? $")
 tip = input("What percentage tip would you like to give? 10, 12  or 15? ")
@@ -25,4 +15,3 @@ calculation2 = float(calculation1) / int(people)
 result = round(calculation2, 2)
 
 print(f"Each person should pay: ${result}")
-
